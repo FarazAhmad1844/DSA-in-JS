7a1140c105c4fd599d8bee5f433a26a155106990
Faraz Ahmad <farazahmad20arid1844@gmail.com>
2024-08-13 17:59:44 +0500

    binary Search with itrative and recursive approach

--- binarysearch.js
+++ binarysearch.js
@@ -1,13 +1,49 @@
+// // binary Search Iterative
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const bs = (arr, target) => {
 
-const arr=[1,2,3,4,5,6,7,8,9,10];
+//     let start = 0;
+//     let end = arr.length - 1; 
+//     while (start <= end) {
+//         let mid = Math.floor((start + end) / 2);
+//         if (arr[mid] === target) {
+//             return mid;
+//         }
+//         else if (arr[mid] > target) {
+//             end = mid - 1;
+//         }
+//         else {
+//             start = mid + 1;
+//         }
 
-function bs(target){
-let left=0;
-let right=arr.length;
-let middle=arr.length/2;
-for(let i=0; i<arr.length; i++){
-    if(middle>)
+//     }
+//     return -1;
+// }
+
+// // console.log(bs([1,2,3,4,5,6,7,8], 1))
+
+
+// Binary Search Recursive
+
+const binarySearchRecu = (arr, target) => {
+    return searchUntil(arr, target, 0, arr.length - 1);
 }
 
+const searchUntil = (arr, target, start, end) => {
+    if (start > end) 
+        return -1;
 
+    let mid = Math.floor((start + end) / 2);
+    
+    if (arr[mid] === target) {
+        return mid;
+    }
+    else if (arr[mid] > target) {
+        return searchUntil(arr, target, start, mid - 1);
+    }
+    else {
+        return searchUntil(arr, target, mid + 1, end);
+    }
 }
+
+console.log(binarySearchRecu([1, 2, 3, 4, 5, 6, 7, 8], 3)); 
